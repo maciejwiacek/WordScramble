fce4988812dbde91a399bda060c0e0b211252d36
Maciej Wiącek <maciejwiacek4@gmail.com>
2023-08-19 16:48:39 +0200

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Maciej Wiącek on 19/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
